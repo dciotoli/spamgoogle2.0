57c7e20b7cd03708c3f5331e23ded50e877ce959
James Wu <inept@wireless-10-104-156-60.umd.edu>
2017-05-05 10:24:37 -0400

    using difference now

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,8 +1,8 @@
 function rating(data){
   var sumIncline=0;
-  for(i = 0;i<data.length;i++){
+  for(i = 1;i<data.length;i++){
     if(data[i]>0){
-      sumIncline+=data[i];
+      sumIncline+=data[i]+data[i-1];
     }
   }
   return sumIncline;
